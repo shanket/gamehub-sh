04112cabff4984803798ae4f05696cccbd8b00ba
Shanket Patel <shanketp@gmail.com>
2025-06-11 22:12:53 -0700

    Created Critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import type { Game } from "../hooks/useGames";
-import { Card, Heading, Image } from "@chakra-ui/react";
+import { Card, Heading, Image, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface GameProps {
   game: Game;
@@ -12,9 +13,12 @@ const GameCard = ({ game }: GameProps) => {
       <Image src={game.background_image}></Image>
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        ></PlatformIconList>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          ></PlatformIconList>
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
       </Card.Body>
     </Card.Root>
   );
