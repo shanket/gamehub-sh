6e2d1637d2a8b1d842f650f0e1a100b8c82bee17
Shanket Patel <shanketp@gmail.com>
2025-06-14 00:56:15 -0700

    CSS Fixex & adding new elements to GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,12 @@
+import { Fragment } from "react";
 import {
   HStack,
   ListItem,
   ListRoot,
   Image,
-  Button,
+  Link,
   Spinner,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { type Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -21,27 +23,35 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <ListRoot listStyleType="none">
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-              variant="plain"
-              fontWeight={genre.id == selectedGenre?.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </ListRoot>
+    <Fragment>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <ListRoot listStyleType="none">
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Link
+                onClick={() => onSelectGenre(genre)}
+                fontSize="md"
+                variant="plain"
+                fontWeight={genre.id == selectedGenre?.id ? "bold" : "normal"}
+                whiteSpace="normal"
+                textAlign="left"
+              >
+                {genre.name}
+              </Link>
+            </HStack>
+          </ListItem>
+        ))}
+      </ListRoot>
+    </Fragment>
   );
 };
 
