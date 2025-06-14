2176d4d9253803df461bb9197825872089129648
Shanket Patel <shanketp@gmail.com>
2025-06-14 01:29:26 -0700

    Ship Genres & Platforms witht the app

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,6 +1,9 @@
-import useData from "./useData";
-import type { Platform } from "./useGames";
+// import useData from "./useData";
+// import type { Platform } from "./useGames";
 
-const usePlatforms = ()=>useData<Platform>("/platforms/lists/parents");
+// const usePlatforms = ()=>useData<Platform>("/platforms/lists/parents");
+
+import platforms from "../data/platforms"
+const usePlatforms = ()=> ({data:platforms,isLoading:false,error:null});
 
 export default usePlatforms;
