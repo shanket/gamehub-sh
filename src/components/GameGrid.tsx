124f49ec41ce3999d6c4a44b5bec9021f3f9f0a4
Shanket Patel <shanketp@gmail.com>
2025-06-20 13:09:44 -0700

    Implement infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,56 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text, Button, Box } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import type { GameQuery } from "../App";
+import React from "react";
 
 interface GameGridProps {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: GameGridProps) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   if (error) return <Text>{error.message}</Text>;
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding="10px"
-      gap="40px"
-    >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
-          </GameCardContainer>
-        ))}
-      {data?.results.map((game) => (
+    <Box padding="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap="40px">
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {/* {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      ))} */}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
+        ))}
+      </SimpleGrid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} marginY={5}>
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </Button>
+      )}
+    </Box>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,9 @@
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import type { GameQuery } from "../App";
 // import apiClient from "../services/api-client";
 import type { FetchResponse } from "../services/api-client";
 import type { Platform } from "./usePlatform";
-
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Game>("/games");
@@ -33,16 +32,26 @@ export interface Game {
 //   //initialData: {count: 0, results: []} // Initial data from local
 // })
 
-const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>,Error>({
-  queryKey: ["games", gameQuery],
-  queryFn: () => apiClient.getAll({ params:{ 
-    genres:gameQuery?.genre?.id,
-    parent_platforms:gameQuery?.platform?.id,
-    ordering:gameQuery?.sortOrder,
-    search:gameQuery?.searchText
-  }}),
-  //staleTime: 24 * 60 * 60 * 1000, // 24 hours
-  //initialData: {count: 0, results: []} // Initial data from local
-})
+const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,Error>(
+  {
+    queryKey: ["games", gameQuery],
+    queryFn: ({ pageParam = 1 }) => apiClient.getAll(
+      { params:
+        { 
+          genres:gameQuery?.genre?.id,
+          parent_platforms:gameQuery?.platform?.id,
+          ordering:gameQuery?.sortOrder,
+          search:gameQuery?.searchText,
+          page: pageParam
+        }
+      }),  
+    getNextPageParam: (lastPage, allPages) => {
+      // Assuming the API returns a 'next' property (URL or boolean)
+      return (lastPage.next ? allPages.length + 1 : undefined);
+    }, 
+    //staleTime: 24 * 60 * 60 * 1000, // 24 hours  
+    initialPageParam: 1
+  }
+)
 
 export default useGames;
