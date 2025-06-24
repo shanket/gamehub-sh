3c999af3db66a1e019f1570f71b86dc7ab487a07
Shanket Patel <shanketp@gmail.com>
2025-06-23 19:31:51 -0700

    Build game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import type { Trailer } from '../entities/Trailer';
+import type { FetchResponse } from '../services/api-client';
+
+const useTrailers = (gameId:number) =>{ 
+
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+  return useQuery<FetchResponse<Trailer>, Error>({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+   });
+}
+
+
+export default useTrailers;
